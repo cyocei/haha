0fe414c41486123ad1dbd83bb850cf42751390d0
cyocei <guleyroblox@gmail.com>
2025-03-18 23:13:16 +1000

    Update server.py

--- server.py
+++ server.py
@@ -133,7 +133,7 @@ async def fetch(session, url, e_string, m_string, e_code, m_code):
     }
 
     try:
-        async with session.get(url, headers=headers, timeout=2, ssl=False) as response:
+        async with session.get(url, headers=headers, timeout=0.8, ssl=False) as response:
             text = await response.text()
 
             if response.status == e_code and e_string in text:
