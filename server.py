9de6c0a626d99d658bec90ecda293ced80cd0b39
cyocei <guleyroblox@gmail.com>
2025-03-21 03:02:38 +1000

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 REQUEST_TIMEOUT = 0.88
-THREAD_POOL_SIZE = 35
+THREAD_POOL_SIZE = 8
 thread_pool = ThreadPoolExecutor(max_workers=THREAD_POOL_SIZE)
 
 USER_AGENTS = [
