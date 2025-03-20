bfc03938eaa449356160fa393d85799da2a0341f
cyocei <guleyroblox@gmail.com>
2025-03-21 02:48:56 +1000

    Update server.py

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-REQUEST_TIMEOUT = 2.45
-THREAD_POOL_SIZE = 8
+REQUEST_TIMEOUT = 1.65
+THREAD_POOL_SIZE = 17
 thread_pool = ThreadPoolExecutor(max_workers=THREAD_POOL_SIZE)
 
 USER_AGENTS = [
