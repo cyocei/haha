2d96a54e59ccba2a6b4e306e867d199872d5bdfc
cyocei <guleyroblox@gmail.com>
2025-03-20 18:18:06 +1000

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-REQUEST_TIMEOUT = 2.2  
+REQUEST_TIMEOUT = 2  
 
 USER_AGENTS = [
    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
